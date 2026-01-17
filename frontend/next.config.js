233fdfabecf64a85adbd54c6fd59e5601304c3fc
Abhay <abhaykrishna280@gmail.com>
2026-01-17 12:13:01 +0530

    Add netlify.toml config and update next.config.js for prod

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,9 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*` 
+          : 'http://localhost:8000/:path*',
       },
     ];
   },
